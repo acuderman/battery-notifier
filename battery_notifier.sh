2a4be0ea0bc4378b6cd4d1f87c98f80280e58210
Andraz Cuderman <cuderman.andraz@gmail.com>
2020-01-24 20:37:19 +0100

    bugfix: match only battery percentages

--- battery_notifier.sh
+++ battery_notifier.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-getBatteryPercentage="acpi | (grep -o '[0-9][0-9]')";
+getBatteryPercentage="acpi | (grep -o '[0-9][0-9]') | head -1";
 previousBattery=$(eval $getBatteryPercentage);
 
 while (true) 
 do
   currentBattery=$(eval $getBatteryPercentage);
-
+  echo "current $currentBattery";
+  echo "previousBattery $previousBattery";
   if [[ $previousBattery -gt $currentBattery && $currentBattery -lt 20 ]]; then
     notify-send "Low battery $currentBattery%";
   fi
-  let previousBattery=$currentBattery;
-  sleep 5m;
+ let previousBattery=$currentBattery;
+ sleep 5m;
 done
 
 
