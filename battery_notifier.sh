a58785b4a82e90c132fc07c177c6aa6a487052b5
Andraz Cuderman <cuderman.andraz@gmail.com>
2020-01-24 20:40:04 +0100

    bugfix: match only battery percentages

--- battery_notifier.sh
+++ battery_notifier.sh
@@ -5,11 +5,11 @@ previousBattery=$(eval $getBatteryPercentage);
 while (true) 
 do
   currentBattery=$(eval $getBatteryPercentage);
-  echo "current $currentBattery";
-  echo "previousBattery $previousBattery";
+
   if [[ $previousBattery -gt $currentBattery && $currentBattery -lt 20 ]]; then
     notify-send "Low battery $currentBattery%";
   fi
+
  let previousBattery=$currentBattery;
  sleep 5m;
 done
