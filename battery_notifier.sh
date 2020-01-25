7b5185b7a785781b9333a44e95877fc49b5aa954
Andraz Cuderman <cuderman.andraz@gmail.com>
2020-01-25 12:57:32 +0100

    add export display, fix so you can run as a aservice

--- battery_notifier.sh
+++ battery_notifier.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+export DISPLAY=:0
+
 while (true) 
 do
   battery=$(acpi | grep -o '[0-9]*%' | grep -o '[0-9]*');
   discharging=$(acpi -b | grep -c "Discharging");
 
-  if [[ $discharging -eq 1 && $battery -lt 20 ]]; then
+  if [[ $discharging -eq 1 && $battery -lt 50 ]]; then
     notify-send "Warning: Low battery $battery%";
   fi
 
