94f44c6a17715da44f619e0d41f7827ac64200d2
Andraž Cuderman <cuderman.andraz@gmail.com>
2020-03-05 21:17:48 +0100

    delete sleep so It can be used with cron job

--- battery_notifier.sh
+++ battery_notifier.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 export DISPLAY=:0
 
-while (true) 
-do
-  battery=$(acpi | grep -o '[0-9]*%' | grep -o '[0-9]*');
-  discharging=$(acpi -b | grep -c "Discharging");
+battery=$(acpi | grep -o '[0-9]*%' | grep -o '[0-9]*');
+discharging=$(acpi -b | grep -c "Discharging");
 
-  if [[ $discharging -eq 1 && $battery -lt 20 ]]; then
-    notify-send -u critical "Warning: Low battery $battery%";
-  fi
-
- sleep 5m;
-done
+if [[ $discharging -eq 1 && $battery -lt 20 ]]; then
+  notify-send -u critical "Warning: Low battery $battery%";
+fi
 
 
